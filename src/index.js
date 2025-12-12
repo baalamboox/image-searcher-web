8707214d4b339e360d06dc66cf1f0c7d44a4e8e3
Guillermo Jimenez <guillermo.jimenez@macstore.mx>
2025-12-11 19:00:41 -0600

    Project started

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import { mkdir,  } from "node:fs/promises";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import * as fs from "node:fs";
+import * as stream from "stream";
+import { promisify } from "util";
+
+const finished = promisify(stream.finished);
+
+const downloadResources = async ({ urlResouce, pathToSave }) => {
+   
+    const writer = fs.createWriteStream(pathToSave, { mode: 0o777 });
+
+    try {
+        const response = await axios.get(urlResouce, {
+            responseType: "stream"
+        });
+        response.data.pipe(writer);
+        await finished(writer);
+    } catch (error) {
+        console.error('Error downloading the image:', error);
+    }
+}
+
+
+
+const createDirectoriesDownload = async ({ listURLsTarget, dirName }) => {
+
+    const listURLs = await listURLsTarget;
+
+    listURLs.urlsResourcesTarget.forEach(async (urlTarget) => {
+        let arrayURLTargetWithoutLastItem = urlTarget.split("/");
+        let nameFile = arrayURLTargetWithoutLastItem.pop();
+        let pathFile = "";
+
+        arrayURLTargetWithoutLastItem.forEach((x) => {
+            pathFile += x + "/";
+        });
+
+        try {
+            const folderPath = new URL(`./${ dirName }/${ pathFile }`, import.meta.url);
+            const createDir = await mkdir(folderPath, { recursive : true, mode: 0o777 });
+
+            await downloadResources({ urlResouce : listURLs.urlToFindDomain + urlTarget, pathToSave : `./src/${ dirName }/${ pathFile + nameFile }` });
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    });
+
+}
+
+const getMediaResources = async ({
+    urlTarget,
+    attrsTarget,
+    urlToFind,
+}) => {
+    try {
+        const response = await axios.get(urlTarget);
+
+        const $ = cheerio.load(response.data);
+
+        let urlsResourcesTarget = [];
+        const urlToFindDomain = urlToFind.split("/")[0] + "//" + urlToFind.split("/")[2] + "/"; 
+
+        attrsTarget.forEach(attr => {
+            $(`*[${ attr }^=${ urlToFind }]`).map((_, element) => {
+                if(attr === "srcset") {
+                    let elementAttrURLS = element.attribs[attr].split(",");
+                    elementAttrURLS.length > 1 ? [
+                        elementAttrURLS.forEach((elementAttrURL) => {
+                            urlsResourcesTarget.push(elementAttrURL.replace(urlToFindDomain, "").replaceAll("//", "/").trim());
+                        })
+                    ] : [
+                        urlsResourcesTarget.push(element.attribs[attr].replace(urlToFindDomain, "").replaceAll("//", "/").trim())
+                    ];
+                } else {
+                    urlsResourcesTarget.push(element.attribs[attr].replace(urlToFindDomain, "").replaceAll("//", "/").trim());
+                }
+            });
+        });
+    
+        return {
+            urlsResourcesTarget,
+            urlToFindDomain
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+createDirectoriesDownload({
+    listURLsTarget : getMediaResources({
+        urlTarget : "https://www.pexels.com/es-es/",
+        urlToFind : "https://images.pexels.com/photos/",
+        attrsTarget : ["src", "srcset", "href"],
+        
+    }),
+    dirName: "pexels"
+});
+
